ebb8f5a68bec2a3e673f08213be44f490b6fce00
adynata <clairewoods@gmail.com>
2015-09-28 17:31:41 -0700

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :reservoirs
+
+  resources :levels
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
