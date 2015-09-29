f20527aacbff8ba2b2ecd43c876ebbcb51ece4f2
adynata <clairewoods@gmail.com>
2015-09-28 17:18:13 -0700

    seed file to create reservoirs and levels in place

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911233232) do
+ActiveRecord::Schema.define(version: 20150928234659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150911233232) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "elevation",       null: false
+    t.string   "operator"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,53 +5,115 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'nokogiri'
+require 'open-uri'
 require 'csv'
 
 
+# This returns only the stations that have daily levels taken
 
-oro_reservoir = Reservoir.new(
-  id: nil,
-  name: "Lake Oroville",
-  station_id: "ORO",
-  hydrologic_area: "Sacramento River",
-  river_basin: "Feather River",
-  location: "Oroville, CA",
-  max_capacity: "3,537,577",
-  county: "Butte",
-  lat: "39.54000",
-  lon: "-121.49300",
-  created_at: nil,
-  updated_at: nil,
-  elevation: 900
-)
+@res_doc = Nokogiri::HTML(open("http://cdec.water.ca.gov/cgi-progs/reservoirs/RES
+"))
 
-oro_reservoir.save!
+@select_station_ids = []
 
-
-file = "db/ORO.csv"
-
-data = CSV.read(file)[2 .. -1]
-
-data.each do |row|
-
-  date = Date.strptime row[0], '%m/%d/%Y'
-  reservoir_id = Reservoir.find_by(station_id: "ORO").id
-
-  level = Level.new(
-    reservoir_id: reservoir_id,
-    level: row[3],
-    date: date)
-
-  level.save
+@res_doc.css('tr').map do |row|
+  station = row.xpath('./td').map(&:text)[1]
+  if station != nil && (station.length == 3)
+    @select_station_ids << station.strip
+  end
 end
 
-# data.each do |row|
-#   if firstrow
-#     date = Date.strptime row[0], '%m/%d/%Y'
-#     storage = row[3]
-#     p "storage is #{storage}, date is #{date}"
-#     # p date.to_date
-#     p date.class
-#     firstrow = false
-#   end
-# end
+puts "The count of all station ids is #{@select_station_ids.count}"
+
+# next, we use all of the station ids to generate profiles for all of the major reservoirs listed
+
+@select_station_ids.each do |station|
+  # gets most of the information specific to each reservoir
+  @reservoir_page = Nokogiri::HTML(open("http://cdec.water.ca.gov/cgi-progs/stationInfo?station_id=" + station))
+
+  full_row_data = @reservoir_page.css('tr').xpath('./td').map(&:text)
+
+  # the last essential data element is capacity, which is housed on a separate page
+
+  reservoir_capacity_doc = Nokogiri::HTML(open("http://cdec.water.ca.gov/cgi-progs/profile?s="+ station +"&type=res"))
+
+  reservoir_name = @reservoir_page.css('h2')[0].text.split.map(&:capitalize).join(' ')
+  reservoir_capacity = reservoir_capacity_doc.css('tr')[1].xpath('./td').map(&:text)[3][0..-3].delete(',').to_i
+  station_id = station
+  longitude = full_row_data[15]
+  city = full_row_data[11].split.map(&:capitalize).join(' ')
+  county = full_row_data[7].split.map(&:capitalize).join(' ')
+  elevation = full_row_data[3]
+  river_basin = full_row_data[5].split.map(&:capitalize).join(' ')+"iver"
+  hydrologic_area = full_row_data[9].split.map(&:capitalize).join(' ')
+  latitude = full_row_data[13]
+  operator = full_row_data[17]
+
+  reservoir = Reservoir.new(
+    name: reservoir_name,
+    station_id: station_id,
+    hydrologic_area: hydrologic_area,
+    river_basin: river_basin,
+    location: city,
+    max_capacity: reservoir_capacity,
+    county: county,
+    lat: latitude,
+    lon: longitude,
+    elevation: elevation,
+    operator: operator
+  )
+  reservoir.save!
+
+  puts "The name of the reservoir is #{reservoir_name}"
+  puts "The capacity is #{reservoir_capacity}"
+  puts "the station id is #{station_id}"
+  puts "The Longitude is #{longitude} & the latitude is #{latitude}"
+  puts "The nearest city is #{city}, in #{county} county"
+  puts "elevation: #{elevation}, river_basin: #{river_basin}"
+  puts "hydrologic_area: #{hydrologic_area}"
+  puts "operator: #{operator}"
+
+
+end
+
+# Finally, we use the station ids to lookup the daily records for levels of each reservoir
+# NOTE: the date in the query string is currently hard coded because I am being lazy and don't want to think about string manipualtion today.
+
+#This is for testing purposes only, please comment it out!
+# @select_station_ids = ["DON", "ORO"]
+
+
+number_of_days = "365"
+
+@select_station_ids.each do |station|
+  @doc = Nokogiri::HTML(open("http://cdec.water.ca.gov/cgi-progs/queryDaily?" + station + "&d=25-Sept-2015+11:19&span=" + number_of_days + "days"))
+
+  # current_storage = @doc.css('tr').xpath('./td').map(&:text)[17].to_i
+  # date = @doc.css('tr').xpath('./td').map(&:text)[16]
+
+  levels_arr = []
+  @doc.css('tr').each do |row|
+    one_entry = []
+    date = row.xpath('./td').map(&:text)[0]
+    level = row.xpath('./td').map(&:text)[1]
+    one_entry << date
+    one_entry << level
+    levels_arr << one_entry
+  end
+
+  # some of the pages returned have tables that are formatted differently, so slightly more is clipped from the data to ensure that what's returned is only levels.
+
+  levels_arr = levels_arr[3..-1]
+
+  # create a level in the db for each pair:
+
+  levels_arr.each do |pair|
+    Level.create(reservoir_id: Reservoir.find_by(station_id: station).id,
+                date: Date.strptime pair[0], '%m/%d/%Y'
+                level: pair[1])
+  end
+
+
+end
