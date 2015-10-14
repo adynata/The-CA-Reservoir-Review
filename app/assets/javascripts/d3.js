75ecb23ebf8884bfa3ed39f720bc94e47ffbacee
Paula Chiu <chiupaula@gmail.com>
2015-10-14 01:34:59 -0700

    Add a chart that uses daily_by_year data

--- app/assets/javascripts/d3.js
+++ app/assets/javascripts/d3.js
@@ -1,124 +1,126 @@
-var width = 500,
-    height = 600,
-    active = d3.select(null);
-var projection = d3.geo.albers()
-    .scale(3000)
-    .translate([width / 2.5, height / 2.5])
-    .parallels([34, 40.5])
-    .rotate([120,0]);
-var path = d3.geo.path()
-    .projection(projection);
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-svg.append("rect")
-    .attr("class", "background")
-    .attr("width", width)
-    .attr("height", height)
-    .on("click", reset);
-
-
-// counties
-var co = svg.append("g").attr("id", "county");
-
-// d3.json("/json/caCountiesTopo.json", function(error, ca) {
-//   if (error) throw error;
-  // svg.selectAll("path")
-  //     .data(topojson.feature(ca, ca.objects.subunits).features)
-  //   .enter().append("path")
-  //     .attr("d", path)
-  //     .attr("class", "county");
-  //     // .on("click", clicked);
-
-  co.append("path")
-      .datum(topojson.mesh(caCountiesTopo, caCountiesTopo.objects.subunits, function(a, b) { return a !== b; }))
-      .attr("class", "mesh")
-      // .attr("class", "county-line")
-      .attr("d", path);
-// });
-
-// statelines
-var a = svg.append("a")
-    .style("stroke-width", "1px");
-
-// d3.json("/json/us.json", function(error, us) {
-//   if (error) throw error;
-
-  a.selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path)
-      .attr("fill-opacity", "0.4")
-      .attr("class", "states");
-      // .on("click", clicked);
-
-  a.append("path")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "mesh")
-      .attr("d", path);
-// });
+$(document).ready(function() {
 
 
 
-//hydrologic regions
-var g = svg.append("g")
-          .style("stroke-width", "1.5px");
+  var width = 500,
+      height = 600,
+      active = d3.select(null);
+  var projection = d3.geo.albers()
+      .scale(3000)
+      .translate([width / 2.5, height / 2.5])
+      .parallels([34, 40.5])
+      .rotate([120,0]);
+  var path = d3.geo.path()
+      .projection(projection);
+  var svg = d3.select("#map").append("svg");
+  svg.append("rect")
+      .attr("class", "background")
+      .on("click", reset);
 
-// d3.json("/json/hydro_regions.json", function(error, hr) {
-//   if (error) throw error;
-  g.selectAll("path")
-      .data(topojson.feature(hydro_regions, hydro_regions.objects.hydrologic_regions_ca).features)
+
+  // counties
+  var co = svg.append("g").attr("id", "county");
+
+  // d3.json("/json/caCountiesTopo.json", function(error, ca) {
+  //   if (error) throw error;
+    // svg.selectAll("path")
+    //     .data(topojson.feature(ca, ca.objects.subunits).features)
+    //   .enter().append("path")
+    //     .attr("d", path)
+    //     .attr("class", "county");
+    //     // .on("click", clicked);
+
+    co.append("path")
+        .datum(topojson.mesh(caCountiesTopo, caCountiesTopo.objects.subunits, function(a, b) { return a !== b; }))
+        .attr("class", "mesh")
+        // .attr("class", "county-line")
+        .attr("d", path);
+  // });
+
+  // statelines
+  var a = svg.append("a")
+      .style("stroke-width", "1px");
+
+  // d3.json("/json/us.json", function(error, us) {
+  //   if (error) throw error;
+
+    a.selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
       .enter().append("path")
-      .attr("d", path)
-      .attr("class", "hydro_reg")
-      .attr("fill-opacity", "0.4")
-      .on("click", clicked);
+        .attr("d", path)
+        .attr("fill-opacity", "0.4")
+        .attr("class", "states");
+        // .on("click", clicked);
 
-  g.append("path")
-      .datum(topojson.mesh(hydro_regions, hydro_regions.objects.hydrologic_regions_ca, function(a, b) { return a !== b; }))
-      .attr("class", "mesh")
-      .attr("d", path);
-
-// });
-
+    a.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("class", "mesh")
+        .attr("d", path);
+  // });
 
 
-// station point data
-var sta = svg.append("g").attr("id", "sta").style("stroke-width", ".5px");
-sta.selectAll("path")
-  .data(sta_json.features)
-  .enter()
-  .append( "path" )
-  .attr( "d", path.pointRadius(3) )
-  .attr("class", "station")
-  // mouseover functions
-  .on("mouseover", function(feature) {
-    var props = feature.properties;
-    console.log(feature)
-    div.transition()
-      .duration(200)
-      .style("opacity", .9);
-    div.html('Name: ' + props.name, 'County: ' + props.county)
-      .style("left", (d3.event.pageX) + 10 + "px")
-      .style("top", (d3.event.pageY - 30) + "px");
-  })
-  .on("mouseout", function(d) {
-    div.transition()
-      // .duration(500)
-      .style("opacity", 0.0);
-  })
-  .on("click", clickedSta);
 
-  // tooltip information
-  var div = d3.select("#map").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+  //hydrologic regions
+  var g = svg.append("g")
+            .style("stroke-width", "1.5px");
 
-// var zoom = d3.behavior.zoom()
-//     .translate([0, 0])
-//     .scale(1)
-//     .scaleExtent([1, 8])
-//     .on("zoom", zoomed);
+  // d3.json("/json/hydro_regions.json", function(error, hr) {
+  //   if (error) throw error;
+    g.selectAll("path")
+        .data(topojson.feature(hydro_regions, hydro_regions.objects.hydrologic_regions_ca).features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("class", "hydro_reg")
+        .attr("fill-opacity", "0.4")
+        .on("click", clicked);
+
+    g.append("path")
+        .datum(topojson.mesh(hydro_regions, hydro_regions.objects.hydrologic_regions_ca, function(a, b) { return a !== b; }))
+        .attr("class", "mesh")
+        .attr("d", path);
+
+  // });
+
+
+
+  // station point data
+  var sta = svg.append("g").attr("id", "sta").style("stroke-width", ".5px");
+  sta.selectAll("path")
+    .data(sta_json.features)
+    .enter()
+    .append( "path" )
+    .attr( "d", path.pointRadius(3) )
+    .attr("class", "station")
+    // mouseover functions
+    .on("mouseover", function(feature) {
+      var props = feature.properties;
+      console.log(feature)
+      div.transition()
+        .duration(200)
+        .style("opacity", .9);
+      div.html('Name: ' + props.name, 'County: ' + props.county)
+        .style("left", (d3.event.pageX) + 10 + "px")
+        .style("top", (d3.event.pageY - 30) + "px");
+    })
+    .on("mouseout", function(d) {
+      div.transition()
+        // .duration(500)
+        .style("opacity", 0.0);
+    })
+    .on("click", clickedSta);
+
+    // tooltip information
+    var div = d3.select("#map").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+  // var zoom = d3.behavior.zoom()
+  //     .translate([0, 0])
+  //     .scale(1)
+  //     .scaleExtent([1, 8])
+  //     .on("zoom", zoomed);
+
+});
 
 function clicked(d) {
   if (active.node() === this) return reset();
--- app/controllers/reservoirs_controller.rb
+++ app/controllers/reservoirs_controller.rb
@@ -1,6 +1,8 @@
 class ReservoirsController < ApplicationController
   def index
     @reservoir_list = Reservoir.all
+    @regions = ["Central Coast", "Colorado River", "North Coast", "North Lahontan", "Sacramento River", "San Francisco Bay", "San Joaquin River", "South Coast", "South Lahontan", "Tulare Lake"]
+    @stations = ["Trinity Lake", "Whiskeytown Dam", "Lewiston", "Warm Springs", "Coyote", "Shasta Dam", "Keswick Reservoir", "Oroville Dam", "Antelope Lake", "Frenchman Dam", "Lake Davis", "New Bullards Bar", "Englebright", "Folsom Lake", "Union Valley", "Loon Lake", "Ice House", "Lake Natoma", "Indian Valley", "Black Butte", "New Hogan Lake", "Camanche Reservoir", "Pardee", "Donnells", "Beardsley Lake", "Tulloch", "New Melones", "Don Pedro Reservoir", "Hetch Hetchy", "Cherry Valley Dam", "New Exchequer-lk Mcclure", "Buchanan Dam", "Hidden Dam", "Friant Dam", "San Luis Reservoir", "Pine Flat Dam", "Terminus Dam", "Success Dam", "Isabella Dam", "Stampede", "Independence Lake", "Donner Lake", "Cachuma Lake", "Pyramid", "Castaic", "Perris"]
     render :index
   end
 
@@ -20,49 +22,48 @@ class ReservoirsController < ApplicationController
     render json: coords
   end
 
+  def daily_by_year
+    reservoir = Reservoir.find(params[:id])
+    levels = reservoir.daily_by_year(params[:year])
+    render json: levels
+  end
 
-    def daily_by_year
-      reservoir = Reservoir.find(params[:id])
-      levels = reservoir.daily_by_year(params[:year])
-      render json: levels
-    end
+  def daily_by_range
+    reservoir = Reservoir.find(params[:id])
+    levels = reservoir.daily_by_range(params[:year1], params[:year2])
+    render json: levels
+  end
 
-    def daily_by_range
-      reservoir = Reservoir.find(params[:id])
-      levels = reservoir.daily_by_range(params[:year1], params[:year2])
-      render json: levels
-    end
+  def average_by_year
+    reservoir = Reservoir.find(params[:id])
+    year = params[:year]
+    render json: reservoir.average_by_year(year)
+  end
 
-    def average_by_year
-      reservoir = Reservoir.find(params[:id])
-      year = params[:year]
-      render json: reservoir.average_by_year(year)
-    end
+  def average_by_range
+    reservoir = Reservoir.find(params[:id])
+    year1 = params[:year1]
+    year2 = params[:year2]
+    render json: reservoir.average_by_range(year1, year2)
+  end
 
-    def average_by_range
-      reservoir = Reservoir.find(params[:id])
-      year1 = params[:year1]
-      year2 = params[:year2]
-      render json: reservoir.average_by_range(year1, year2)
-    end
+  def by_hydrologic
+    parsed_hr = params[:hr].split(/(?<=[a-z])(?=[A-Z])/).join(' ')
+    stations_by_hr = Reservoir.where("hydrologic_area = ?", parsed_hr)
+    render json: stations_by_hr
+  end
 
-    def by_hydrologic
-      parsed_hr = params[:hr].split(/(?<=[a-z])(?=[A-Z])/).join(' ')
-      stations_by_hr = Reservoir.where("hydrologic_area = ?", parsed_hr)
-      render json: stations_by_hr
-    end
-
-    def monthly_by_year
-      reservoir = Reservoir.find(params[:id])
-      year = params[:year]
-      render json: reservoir.monthly_by_year(year)
-    end
+  def monthly_by_year
+    reservoir = Reservoir.find(params[:id])
+    year = params[:year]
+    render json: reservoir.monthly_by_year(year)
+  end
 
 
-    private
+  private
 
-    def reservoir_params
-      params.require(:reservoir).permit(:id, :year, :year1, :year2)
-    end
+  def reservoir_params
+    params.require(:reservoir).permit(:id, :year, :year1, :year2)
+  end
 
 end
