27180f8601c6207f99459df2aa1ec9d269d05665
adynata <clairewoods@gmail.com>
2015-10-14 16:32:36 -0700

    sad input button

--- app/assets/javascripts/styling_script.js
+++ app/assets/javascripts/styling_script.js
@@ -22,6 +22,56 @@ $(".dropdown_station dd ul li a").on('click', function(e) {
   e.stopImmediatePropagation();
 });
 
+
+obj.dd.on('click', function(event){
+  $(this).toggleClass('active');
+  return false;
+});
+
+//...
+
+// $(function() {
+
+  var dd = new DropDown( $('#dd') );
+
+  $(document).click(function() {
+    // all dropdowns
+    $('.wrapper-dropdown').removeClass('active');
+  });
+
+// });
+//
+function DropDown(el) {
+    this.dd = el;
+    this.placeholder = this.dd.children('span');
+    this.opts = this.dd.find('ul.dropdown > li');
+    this.val = '';
+    this.index = -1;
+    this.initEvents();
+}
+DropDown.prototype = {
+    initEvents : function() {
+        var obj = this;
+
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('active');
+            return false;
+        });
+
+        obj.opts.on('click',function(){
+            var opt = $(this);
+            obj.val = opt.text();
+            obj.index = opt.index();
+            obj.placeholder.text('Gender: ' + obj.val);
+        });
+    },
+    getValue : function() {
+        return this.val;
+    },
+    getIndex : function() {
+        return this.index;
+    }
+}
 // function getSelectedValue(id) {
 //   return $("#" + id).find("dt a span.value").html();
 // }
